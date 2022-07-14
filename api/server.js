224c6dab3b827e372e48c4da3d0b19c3926a9538
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-14 11:52:59 -0500

    motd feature added

--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ server.get('/', (req, res) => {
     res.status(200).send('<h1>The server is up!</h1>')
 })
 
+server.get('/motd', (req, res) => {
+    res.send(`<h1>Today's Message:</h1><p>${process.env.MOTD || "(no message)"}</p>`)
+})
+
 module.exports = server;
